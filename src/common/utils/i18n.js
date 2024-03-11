5c8eb1fd1d9b50f3862c8cebc17cd1a238d79571
TomJerry <815378559@qq.com>
2024-03-11 10:52:32 +0800

    ++

--- src/common/utils/i18n.js
+++ src/common/utils/i18n.js
@@ -1,7 +1,7 @@
 import {createI18n} from 'vue-i18n'
 
 function loadLocaleMessages() {
-    const locales = import.meta.glob('/public/local/*.json')
+    const locales = import.meta.glob('/public/local/*.json?url')
     const messages = {}
     Object.keys(locales).forEach(key => {
         const res = key.match(/[A-Za-z0-9-_,\s]+(?=\.json$)/i)
