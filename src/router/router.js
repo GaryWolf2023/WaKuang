f0c06ac36279c654795b727929dde698a3d10427
Tom <husky._lby@petalmail.com>
2024-03-12 10:49:43 +0800

    router报错问题解决

--- src/router/router.js
+++ src/router/router.js
@@ -1,18 +1,23 @@
-import VueRouter from 'vue-router'
+import * as VueRouter from 'vue-router'
 
 import Login from '../pages/auth/login.vue'
 import Register from '../pages/auth/register.vue'
 import Home from '../pages/mining/index.vue'
 
 const routes = [
-    { path: '/', component:Home },
-    { path: '/login', component: Login },
-    { path: '/register', component: Register },
+    { path: '/', name: 'home', component:Home },
+    { path: '/login', name: 'login', component: Login },
+    { path: '/register', name: 'login', component: Register },
 ]
 
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes: routes,
 })
+
+router.beforeEach((to, from) => {
+    console.log(to, from);
+    return true
+  })
 
 export default router
