ba4d148b392d84aeb354ceebdd88e52011ec319d
Tom <husky._lby@petalmail.com>
2024-03-15 00:01:22 +0800

    写了login页面，样式没调，写了国际化的一些文件内容

--- src/pages/auth/register.vue
+++ src/pages/auth/register.vue
@@ -1,11 +1,12 @@
 <template>
-    <div>
-
-    </div>
+  <Layout_imp :hid-tab-bar="true">
+register
+  </Layout_imp>
 </template>
 
 <script setup>
 
+import Layout_imp from "@/common/layouts/common/layout_imp.vue";
 </script>
 
 <style lang="scss" scoped></style>
