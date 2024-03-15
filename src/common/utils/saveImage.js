3211539ea17293ea527101885877d06127a99b82
TomJerry <815378559@qq.com>
2024-03-15 14:32:21 +0800

    ++

--- src/common/utils/saveImage.js
+++ src/common/utils/saveImage.js
@@ -4,5 +4,4 @@ export function saveImage() {
     if (false) {
         
     }
-    
 }
--- src/pages/profile/profile.vue
+++ src/pages/profile/profile.vue
@@ -14,11 +14,11 @@
     <div class="nav-bar">
       <div class="nav-item" v-for="(item, index) in menuList" :key="index">
         <router-link :to="item.path" v-if="item.path">
-          <img :src="`./../assets/pages/profile/menu-item_${index + 1}.png`" alt="icon" style="font-size: 8px"/>
+          <img :src="item.icon" alt="icon" style="font-size: 8px;width: 44px"/>
           <p style="margin-top:9px">{{ item.title }}</p>
         </router-link>
         <div v-if="item.action" @lick="handleClick(item.action)">
-          <img :src="`./../assets/pages/profile/menu-item_${index + 1}.png`" alt="icon" style="font-size: 8px"/>
+          <img :src="item.icon" alt="icon" style="font-size: 8px;width: 44px"/>
           <p style="margin-top:9px">{{ item.title }}</p>
         </div>
       </div>
@@ -29,27 +29,38 @@
 <script setup>
 import {useI18n} from "vue-i18n";
 
+import icon1 from "@/assets/pages/profile/menu-item_1.png"
+import icon2 from "@/assets/pages/profile/menu-item_2.png"
+import icon3 from "@/assets/pages/profile/menu-item_3.png"
+import icon4 from "@/assets/pages/profile/menu-item_4.png"
+import icon5 from "@/assets/pages/profile/menu-item_5.png"
+import icon6 from "@/assets/pages/profile/menu-item_6.png"
+import icon7 from "@/assets/pages/profile/menu-item_7.png"
+import icon8 from "@/assets/pages/profile/menu-item_8.png"
+
+
 const { t } = useI18n()
 const menuList = [
-  {title: t("my.myMiner"), path: "/user/miner"},
-  {title: t("my.myEarning"), path: "/user/income"},
-  {title: t("my.myTeam"), path: "/user/team"},
-  {title: t("my.changePassword"), path: "/retrievePassword"},
-  {title: t("my.fundCode"), path: "/retrievePassword"},
-  {title: t("my.systemLanguage"), path: "change-lang"},
-  {title: t("my.feedback"), path: "/feedBack"},
-  {title: t("my.quitSafe"), action: "quit-safe"},
+  {title: t("my.myMiner"), path: "/user/miner", icon: icon1},
+  {title: t("my.myEarning"), path: "/user/income", icon: icon2},
+  {title: t("my.myTeam"), path: "/user/team", icon: icon3},
+  {title: t("my.changePassword"), path: "/retrievePassword", icon: icon4},
+  {title: t("my.fundCode"), path: "/retrievePassword", icon: icon5},
+  {title: t("my.systemLanguage"), action: "change-lang", icon: icon6},
+  {title: t("my.feedback"), path: "/feedBack", icon: icon7},
+  {title: t("my.quitSafe"), action: "quit-safe", icon: icon8},
 ]
 </script>
 
 <script>
 import {defineComponent} from "vue";
 import Layout_imp from "@/common/layouts/common/layout_imp.vue";
+import icon1 from "@/assets/pages/profile/menu-item_1.png";
 
 export default defineComponent({
   components: {Layout_imp},
   methods: {
-    handClick(actionType){
+    handleClick(actionType){
       switch (actionType) {
         case "change-lang":
         case "quit-safe":
@@ -83,6 +94,15 @@ export default defineComponent({
 .nav-bar {
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  color: $input-text;
+  text-align: center;
+  border-radius: 10px;
+  margin-top: 10px;
+  background-color: $input-bg;
+  padding: 24px;
+  grid-gap: 20px;
+  font-size: 13px;
+  a {
+    color: $input-text;
+  }
 }
 </style>
