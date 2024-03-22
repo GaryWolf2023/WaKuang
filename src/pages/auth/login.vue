7e6bdef6ab64173a156a2d07c6e67c5374069410
Tom <husky._lby@petalmail.com>
2024-03-22 17:02:46 +0800

    322(2)

--- src/pages/auth/login.vue
+++ src/pages/auth/login.vue
@@ -2,7 +2,7 @@
     <Layout_imp :hid-tab-bar="true" :hidBack=true>
       <template v-slot:header>
         <div class="reset-header" @click="showDrawer=true">
-          中文
+          {{showLang}}
         </div>
       </template>
       <div class="login-top">
@@ -44,12 +44,29 @@
 
 <script setup>
 import {useRouter} from 'vue-router'
-import {ref, reactive} from 'vue'
+import {ref, reactive, computed} from 'vue'
 
 import LangBar from "@/common/components/base/LangBar.vue";
 import Layout_imp from "@/common/layouts/common/layout_imp.vue";
+import {useAppStore} from "@/pinia/modules/app.js"
 
+const appStore = useAppStore()
 const router = useRouter()
+
+let lang = computed(()=>{
+  return appStore.language
+})
+
+let showLang = computed(() => {
+  let list = appStore.languageList
+  let str = ''
+  list.forEach((it) => {
+    if (it.mark === lang.value) {
+      str = it.name
+    }
+  })
+  return str
+})
 
 let showDrawer = ref(false)
 const handleCloseLang = (val) => {
